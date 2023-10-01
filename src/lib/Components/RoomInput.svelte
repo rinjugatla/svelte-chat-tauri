1098b76451320b05b0de10bbddc5392bc5aa5345
rinjugatla <rin.jugatla@gmail.com>
2023-10-01 21:45:06 +0900

    部屋名の書式を検証

--- src/lib/Components/RoomInput.svelte
+++ src/lib/Components/RoomInput.svelte
@@ -16,24 +16,32 @@
     // 入室
 	let meet_id = '';
 	let posting = false;
-	const submit = async () => {
-		posting = true;
-		const room_firebase_id = await postRoom(meet_id);
-		if (room_firebase_id) {
-			invoke("create_meet_window", {meet: {id: meet_id}});
-			document.location.href = `/rooms/${room_firebase_id}`;
-		} else {
-			alert('ルームの作成に失敗しました。');
-		}
+	const enterRoom = async () => {
+		try{
+			posting = true;
+			const isValidRommId = meet_id.match("[a-z]{3}-[a-z]{4}-[a-z]{3}");
+			if (!isValidRommId){ 
+				alert('部屋IDの形式に誤りがあります。');
+				return; 
+			}
 
-		posting = false;
+			const room_firebase_id = await postRoom(meet_id);
+			if (room_firebase_id) {
+				invoke("create_meet_window", {meet: {id: meet_id}});
+				document.location.href = `/rooms/${room_firebase_id}`;
+			} else {
+				alert('ルームの作成に失敗しました。');
+			}
+		}finally{
+			posting = false;
+		}
 	};
 
 	const submitByKey = async (event: KeyboardEvent) => {
 		const needSubmit = event.key == 'Enter';
 		if(!needSubmit){ return; }
 		
-		await submit();
+		await enterRoom();
 	};
 </script>
 
@@ -42,8 +50,8 @@
         <form class="p-5">
 			<ButtonGroup class="w-full">
 				<InputAddon class="break-keep">部屋名</InputAddon>
-				<Input id="room" type="text" bind:value={meet_id} on:keydown={submitByKey}/>
-				<Button class="break-keep" color="primary" disabled={posting || meet_id.length == 0} on:click={submit}>入室</Button>
+				<Input id="room" type="text" bind:value={meet_id} on:keydown={submitByKey} placeholder="abc-defg-hij" required/>
+				<Button class="break-keep" color="primary" disabled={posting || meet_id.length == 0} on:click={enterRoom}>入室</Button>
 			</ButtonGroup>
         </form>
     {/if}
