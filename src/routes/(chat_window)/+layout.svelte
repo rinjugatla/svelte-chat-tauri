2b30cfc3c9370cfd2be098ff18b4f9b692a12d34
rinjugatla <rin.jugatla@gmail.com>
2023-11-14 15:11:48 +0900

    チャット画面背景の色が全体に適用されていなかった不具合を修正

--- src/routes/(chat_window)/+layout.svelte
+++ src/routes/(chat_window)/+layout.svelte
@@ -4,9 +4,9 @@
 	import Header from '$lib/Common/Header.svelte';
 </script>
 
-<main class="bg-white dark:bg-gray-400">
+<main class="bg-white dark:bg-gray-400 h-screen">
 	<Header />
-	<div class="pt-16 pb-4 z-20">
+	<div class="pt-16 pb-4 z-20 bg-white dark:bg-gray-400">
 		<slot />
 	</div>
 </main>
